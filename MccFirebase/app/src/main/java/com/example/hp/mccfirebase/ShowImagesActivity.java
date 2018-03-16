8b52ac0c6218d79490bf68b0cc7e3d3353e47870
shwetarkadam <shwetarkadam25@gmail.com>
2018-03-16 20:23:38 +0530

    SHows images in a linear manner retrieved from firebase

---
+++ MccFirebase/app/src/main/java/com/example/hp/mccfirebase/ShowImagesActivity.java
@@ -0,0 +1,106 @@
+package com.example.hp.mccfirebase;
+
+import android.provider.SyncStateContract;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import android.app.ProgressDialog;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.util.Log;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.HorizontalScrollView;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
+import android.widget.Toast;
+
+import com.example.hp.mccfirebase.adapter.MyAdapter;
+import com.example.hp.mccfirebase.pojos.Upload;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+import com.squareup.picasso.Picasso;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ShowImagesActivity extends AppCompatActivity {
+    int count;
+    HorizontalScrollView horizontalScrollView;
+    LinearLayout linearLayout;
+    RelativeLayout rl;
+    List<String> imagesUrl = new ArrayList<String>();
+
+    //recyclerview object
+    private RecyclerView recyclerView;
+
+    //adapter object
+    private RecyclerView.Adapter adapter;
+
+    //database reference
+    private DatabaseReference mDatabase;
+
+    //progress dialog
+    private ProgressDialog progressDialog;
+
+    //list to hold all the uploaded images
+    private List<Upload> uploads;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_show_images);
+
+        // to put horizontalscroll
+        //rl = findViewById(R.id.RL);
+        recyclerView = (RecyclerView) findViewById(R.id.recyclerView);
+        recyclerView.setHasFixedSize(true);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+
+
+        progressDialog = new ProgressDialog(this);
+
+        uploads = new ArrayList<>();
+
+        //displaying progress dialog while fetching images
+        progressDialog.setMessage("Please wait...");
+        progressDialog.show();
+        mDatabase = FirebaseDatabase.getInstance().getReference("uploads");
+
+        //adding an event listener to fetch values
+        mDatabase.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot snapshot) {
+                //dismissing the progress dialog
+                progressDialog.dismiss();
+                count = (int) snapshot.getChildrenCount();
+                //iterating through all the values in database
+                for (DataSnapshot postSnapshot : snapshot.getChildren()) {
+
+
+
+                    Upload upload = postSnapshot.getValue(Upload.class);
+                    uploads.add(upload);
+                }
+                //creating adapter
+                adapter = new MyAdapter(getApplicationContext(), uploads);
+
+                //adding adapter to recyclerview
+                recyclerView.setAdapter(adapter);
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+                progressDialog.dismiss();
+            }
+        });
+
+    }
+
+   }
